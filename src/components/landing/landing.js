d10e7c894afac48a14a19bd2c548920efe25399a
JeffVega <jeff.vega321@gmail.com>
2019-02-25 10:29:35 -0800

    setup react project

---
+++ src/components/landing/landing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Search from '../search-bar/search';
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <Search/>
+      </div>
+    )
+  }
+}
