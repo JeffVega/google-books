6d0bfafa25d4600cef9bd438b44e2af05a7c2768
JeffVega <jeff.vega321@gmail.com>
2019-03-06 16:55:08 -0800

    updated book store

--- src/components/bookCards/book.js
+++ src/components/bookCards/book.js
@@ -8,22 +8,30 @@ class Book extends Component {
             getBooks()
         )
     }
+    books(){
+        if(this.props.book.books){
+        return(
+            
+                this.props.book.books.map((data, index) => (
+                        
+                           <div key={index} className="books">
+                           <img className="books__img" src={data.imageLinks.smallThumbnail} alt={data.title}/>
+                               <div className="books__helper">
+                                   <p className="books__title">{data.title}</p>
+                                   <p className="books__authors">{data.authors.map(x => x)}</p>
+                                   <p className="books__publisher">{data.publisher}</p>
+                                   <a className="books__infoLinks"href={data.infoLink}>Learn More</a>                        
+                               </div>
+                           </div>
+                       ))
+        )}
+    }
   render() {
     
      
     return (
       <div className="bookPage">
-         {
-             this.props.book.books.map((data, index) => (
-                     
-                        <div key={index} className="books">
-                        <img className="books__img" src={data.imageLinks.smallThumbnail} alt={data.title}/>
-                            <p className="books__title">{data.title}</p>
-                            <p className="books__authors">{data.authors.map(x => x)}</p>
-                            <p className="books__publisher">{data.publisher}</p>
-                            <a className="books__infoLinks"href={data.infoLink}>Learn More</a>                        
-                        </div>
-                    ))}
+    {this.books()}
       </div>
     )
   }
